fcd9b1ef1c8af00943bb3a7048d8d2c66585ba1c
YinCY123 <yinchunyou2012@163.com>
2022-02-28 19:53:46 +0800

    removed

--- Python-Crash-Course/chapter10-file-and-exceptions/alice.py
+++
@@ -1,7 +0,0 @@
-filename = "alice.txt"
-
-try:
-    with open(filename) as f_obj:
-        contents = f_obj.read()
-except FileNotFoundError:
-    print("Sorry, the file " + filename + " is not found.")
--- Python-Crash-Course/chapter11-testing-your-code/city_functions.py
+++
@@ -1,3 +0,0 @@
-def cities(city_name, country_name, population):
-    """return the formatted city name"""
-    print(city_name.title() + ", " + country_name.title() + " - population " + str(population))
